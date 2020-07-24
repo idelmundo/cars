b36645d63a29f1bef7c0bb2e9d7af648c06f56f6
Isaias Del Mundo <idelmundo68@gmail.com>
2020-07-23 18:37:24 -0700

    update comments on schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,12 @@
--- Drops the day_planner_db if it already exists --
+-- Drops the cars_db if it already exists --
 DROP DATABASE IF EXISTS cars_db;
 
--- Create the database day_planner_db and specified it for use.
+-- Create the database cars_db and specified it for use.
 CREATE DATABASE cars_db;
 
 USE cars_db;
 
--- Create the table plans.
+-- Create the table cars.
 CREATE TABLE cars (
   id int NOT NULL AUTO_INCREMENT,
   car_name varchar(255) NOT NULL,
