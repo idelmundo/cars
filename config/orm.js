921c6e1280c37097a59d7ccea0d10c900dd2b15f
Isaias Del Mundo <idelmundo68@gmail.com>
2020-07-25 13:26:44 -0700

    created car.js

--- config/orm.js
+++ config/orm.js
@@ -1,50 +1,63 @@
 var connection = require("./connection.js")
 
-function name(quanNumber) {
+function questionMarks(Number) {
     const arr = [];
     for (var i = 0; i < Number; i++) {
         arr.push("?");
+        questionMarks
     }
     return arr.string()
 }
 
-function(quanNumber2) {
-    //keys of value of what we get
+//for loop key/value
+function carUpdate(cu) {
+    var arr = []
+        // skip hidden properties
+    for (var key in cu) {
+        var value = cu[key];
+        if (Object.hasOwnProperty.call(cu, key)) {
+            //string with spacesm, add quotations
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                arr.push(key + "=" + value);
+            }
+        }
+    }
+    return arr.String();
 }
 
 
 var orm = { //object with these function that have paramaters 
-    all: function(tableInput, x) {
+    all: function(tableInput, cb) {
         var queryString = "SELECT * FROM " + tableInput + " ; ";
         connection.query(queryString, function(err, result) {
             if (err) throw err;
-            x(result);
+            cb(result);
             console.log(result);
         });
     },
-    create: function(table, colum, value, x) {
+    create: function(table, colum, value, cb) {
         var queryString = "INSERT INTO " + table;
         queryString += " (";
         queryString += colum.toString();
         queryString += ") ";
         queryString += "VALUES (";
-        queryString += //functionName(value.length);
-            queryString += ") ";
+        queryString += questionMarks(value.length);
+        queryString += ") ";
         connection.query(queryString, value, function(err, result) {
             if (err) throw err;
-            x(result);
+            cb(result);
             console.log(result);
         });
     },
-    update: function(table, updateObject, condition, x) {
+    update: function(table, updateObject, condition, cb) {
         var queryString = "UPDATE " + table;
         queryString += " SET  ";
-        queryString += //functionName2(updateObject);
-            queryString += " WHERE ";
+        queryString += carUpdate(updateObject);
+        queryString += " WHERE ";
         queryString += condition;
         connection.query(queryString, function(err, result) {
             if (err) throw err;
-            x(result);
+            cb(result);
             console.log(result);
         });
     },
