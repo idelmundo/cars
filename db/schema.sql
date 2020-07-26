f3e9632872d9619552f920ebabda7c59684db5e3
Isaias Del Mundo <idelmundo68@gmail.com>
2020-07-26 16:55:27 -0700

    changed index file

--- db/schema.sql
+++ db/schema.sql
@@ -10,7 +10,7 @@ USE cars_db;
 CREATE TABLE cars (
   id int NOT NULL AUTO_INCREMENT,
   car_name varchar(255) NOT NULL,
-  sell boolean NOT NULL,
+  sell boolean DEFAULT FALSE ,
   PRIMARY KEY (id)
 );
 
